f9d4c9e869c636f838a708d548e3d1a429d218b0
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-01 18:01:54 -0400

    User Model function definitions

---
+++ src/model/userModel.js
@@ -0,0 +1,9 @@
+const supabase = require("../config/supabase");
+
+const userModel = {
+  async createUser(email, username, password) {},
+  async findUserByEmail(email) {},
+  async findUserById(id) {},
+  async updateUser(id, data) {},
+  async deleteUser(id) {},
+};
