4e71495f1b7fa682f1ae7e98ef114076f202b945
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-01 19:08:00 -0400

    Env name change

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default supabase;
