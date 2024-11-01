d3d65c2d61065d197697d327871272a3ca201cce
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-01 19:43:59 -0400

    Delete duplicate files

--- src/model/userModel.js
+++
@@ -1,9 +0,0 @@
-const supabase = require("../config/supabase");
-
-const userModel = {
-  async createUser(email, username, password) {},
-  async findUserByEmail(email) {},
-  async findUserById(id) {},
-  async updateUser(id, data) {},
-  async deleteUser(id) {},
-};
