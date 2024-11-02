511b47b35717792959afdb741fb2a70c30406482
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-01 22:45:30 -0400

    Login user functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,14 +2,54 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
+/**
+ * This function creates a new user in the database
+ * @param {*} req The request object
+ * @param {*} res The response object
+ * @returns The created user object and a success message
+ */
 export const signupUser = async (req, res) => {
   const { email, username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
 
   try {
     const user = await userModel.createUser(email, username, hashedPassword);
-    res.status(201).json({ message: "User created successfully", user: user });
+    return res
+      .status(201)
+      .json({ message: "User created successfully", user: user });
   } catch (error) {
-    res.status(500).json({ error: error.message, message: "Can't create user" });
+    return res
+      .status(500)
+      .json({ error: error.message, message: "Can't create user" });
+  }
+};
+
+/**
+ * This function logs in a user by checking the user's credentials
+ * @param {*} req The request object
+ * @param {*} res The response object
+ * @returns A success message and a JWT token
+ */
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findUserByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "User logged in", token: token });
+  } catch (error) {
+    res.status(500).json({ error: error.message, message: "Can't login user" });
   }
 };
