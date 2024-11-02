ec9af071ef811ecb127f7cfa8c766ceddf24e5ae
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-01 23:10:12 -0400

    Convert to single module format

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,54 +2,93 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
-/**
- * This function creates a new user in the database
- * @param {*} req The request object
- * @param {*} res The response object
- * @returns The created user object and a success message
- */
-export const signupUser = async (req, res) => {
-  const { email, username, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+const userController = {
+  /**
+   * This function creates a new user in the database
+   * @param {*} req The request object
+   * @param {*} res The response object
+   * @returns The created user object and a success message
+   */
+  signupUser: async (req, res) => {
+    const { email, username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
-  try {
-    const user = await userModel.createUser(email, username, hashedPassword);
-    return res
-      .status(201)
-      .json({ message: "User created successfully", user: user });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ error: error.message, message: "Can't create user" });
-  }
+    try {
+      const user = await userModel.createUser(email, username, hashedPassword);
+      return res
+        .status(201)
+        .json({ message: "User created successfully", user: user });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: error.message, message: "Can't create user" });
+    }
+  },
+
+  /**
+   * This function logs in a user by checking the user's credentials
+   * @param {*} req The request object
+   * @param {*} res The response object
+   * @returns A success message and a JWT token
+   */
+  loginUserUsingEmail: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await userModel.findUserByEmail(email);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.status(200).json({ message: "User logged in", token: token });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: error.message, message: "Can't login user" });
+    }
+  },
+
+  /**
+   * This function gets a user's data from the database
+   * @param {*} req The request object
+   * @param {*} res The response object
+   * @returns The user object from the database
+   */
+  getUserData: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const user = await userModel.findUserById(id);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.status(200).json({ user: user });
+    } catch (error) {
+      res.status(500).json({ error: error.message, message: "Can't get user" });
+    }
+  },
+
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await userModel.getAllUsers();
+      res.status(200).json({ users: users });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: error.message, message: "Can't get users" });
+    }
+  },
 };
 
-/**
- * This function logs in a user by checking the user's credentials
- * @param {*} req The request object
- * @param {*} res The response object
- * @returns A success message and a JWT token
- */
-export const loginUserUsingEmail = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await userModel.findUserByEmail(email);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    if (!bcrypt.compareSync(password, user.password)) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    res.status(200).json({ message: "User logged in", token: token });
-  } catch (error) {
-    res.status(500).json({ error: error.message, message: "Can't login user" });
-  }
-};
+export default userController;
