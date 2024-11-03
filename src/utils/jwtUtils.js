43313ba2bf9aef74eab316cee840512546697b0d
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-02 23:59:37 -0400

    Export style changed

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,36 +1,31 @@
 import jwt from "jsonwebtoken";
 
 /**
- * This function generates an access token for the user
+ * Generates an access token for the user
  * @param {*} userID The user's ID to generate the access token for
  * @returns The generated access token for the user
  */
-const generateAccessToken = (userID) => {
-  return jwt.sign(userID, process.env.JWT_SECRET, { expiresIn: "1h" });
+export const generateAccessToken = (userID) => {
+  return jwt.sign({ id: userID }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 /**
- * This function generates a refresh token for the user
+ * Generates a refresh token for the user
  * @param {*} userID The user's ID to generate the refresh token for
  * @returns The generated refresh token for the user
  */
-const generateRefreshToken = (userID) => {
-  return jwt.sign(userID, process.env.REFRESH_TOKEN_SECRET, {
+export const generateRefreshToken = (userID) => {
+  return jwt.sign({ id: userID }, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: "30d",
   });
 };
 
 /**
- * This function verifies the user's access token
- * @param {*} accessToken The user's access token
- * @returns The decoded user's access token
+ * Verifies a token with the provided secret
+ * @param {*} token The token to verify
+ * @param {*} secret The secret key to verify the token with
+ * @returns The decoded payload of the token
  */
-const verifyAccessToken = (accessToken, secret) => {
-  return jwt.verify(accessToken, secret);
-};
-
-module.exports = {
-  generateAccessToken,
-  generateRefreshToken,
-  verifyAccessToken,
+export const verifyAccessToken = (token, secret) => {
+  return jwt.verify(token, secret);
 };
