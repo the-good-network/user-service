ba93ec278ff4a2ab2d35641db784ab13e79ffd29
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-03 17:41:40 -0500

    Authenticate using refresh token first

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import { verifyAccessToken } from "../utils/jwtUtils.js";
+import { verifyToken } from "../utils/jwtUtils.js";
 
 /**
  *
@@ -8,18 +8,35 @@ import { verifyAccessToken } from "../utils/jwtUtils.js";
  * @returns Returns if everything is okay, otherwise returns an error message
  */
 export const authenticate = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    // Handle refresh token first from the cookie
+    const refreshToken = req.cookies.refreshToken;
+    
+    if (refreshToken) {
+      const refreshTokenVerification = verifyToken(refreshToken);
+  
+      if (refreshTokenVerification && refreshTokenVerification.type === "refresh") {
+        // Set the user ID in the request object
+        req.user = refreshTokenVerification.payload;
+        return next();
+      }
+    }
+    
+    // If refresh token verification fails, check the access token from the authorization header
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
   
     if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
+      return res.status(401).json({ message: "No token provided" });
     }
   
-    try {
-      const payload = verifyAccessToken(token, process.env.JWT_SECRET);
-      req.user = payload;
-      next();
-    } catch (error) {
-      res.status(403).json({ message: 'Invalid or expired token' });
+    const accessTokenVerification = verifyToken(token);
+  
+    if (accessTokenVerification && accessTokenVerification.type === "access") {
+      // Set the user ID in the request object
+      req.user = accessTokenVerification.payload;
+      return next();
     }
+  
+    // If both tokens are invalid or expired
+    return res.status(403).json({ message: "Invalid or expired token" });
   };
