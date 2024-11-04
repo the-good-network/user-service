c9e2495c88a2963daf92435ade47252c404ad933
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-03 20:17:50 -0500

    Remove refresh-token route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { loginUsingEmail, refreshToken, logout } from "../controllers/authController.js";
+import { loginUsingEmail, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", loginUsingEmail);
-router.post("/refresh-token", refreshToken);
 router.post("/logout", logout);
 
 export default router;
