24e0214b894aa6ee518b9ba6ea77a1abb3937d76
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-05 17:42:49 -0500

    Add user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import userController from "../controllers/userController.js";
+import { admin } from "../middleware/userMiddleware.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", userController.signupUser);
 router.post("/reset-password", userController.resetPassword);
+
+// Admin routes
+router.get("/all", authenticate, admin, userController.getAllUsers);
+router.get("/:id", authenticate, admin, userController.getUser);
 
 export default router;
