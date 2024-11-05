01cf8973dbed0cfe79d7527aef7c18a403aa7e23
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-05 12:32:35 -0500

    Add route for password-reset

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,6 @@ import userController from "../controllers/userController.js";
 const router = express.Router();
 
 router.post("/signup", userController.signupUser);
+router.post("/reset-password", userController.resetPassword);
 
 export default router;
