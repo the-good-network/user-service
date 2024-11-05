dce749d04455465f45e112c48fc6d849c54671cd
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-05 17:53:25 -0500

    Implement get own profile function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -40,6 +40,30 @@ const userController = {
   },
 
   /**
+   * Retrieves the authenticated user's profile
+   * @param {*} req The request object
+   * @param {*} res The response object
+   */
+  getOwnProfile: async (req, res) => {
+    const id = req.id; // User ID from the authenticate middleware
+
+    try {
+      const user = await userModel.findUserById(id);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      return res
+        .status(200)
+        .json({ message: "Retrieved user info", user: user });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Can't get user profile", error: error.message });
+    }
+  },
+
+  /**
    * Resets the user's password
    * @param {*} req The request object
    * @param {*} res The response object
