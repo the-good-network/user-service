eb1147aa392a794d8abbee443992314827c8afeb
Pratim Sadhu <pratimsadhu@icloud.com>
2024-11-04 20:45:09 -0500

    Forgot password controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,10 @@ import {
   generateAccessToken,
   generateRefreshToken,
 } from "../utils/jwtUtils.js";
+import { generateResetCode, validateResetCode } from "../utils/utils.js";
 import userModel from "../models/userModel.js";
+import authModel from "../models/authModel.js";
+import { sendForgotPasswordEmail } from "../controllers/emailController.js";
 
 /**
  * Logs in a user by checking the user's credentials
@@ -17,7 +20,7 @@ export const loginUsingEmail = async (req, res) => {
   try {
     const user = await userModel.findUserByEmail(email);
 
-    if (!user || !bcrypt.compareSync(password, user.password)) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
@@ -45,6 +48,80 @@ export const loginUsingEmail = async (req, res) => {
 };
 
 /**
+ * Sends a reset code to the user's email for password reset
+ * @param {*} req The request object
+ * @param {*} res The response object
+ * @returns A success message
+ */
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await userModel.findUserByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+
+    const resetCode = generateResetCode();
+    await authModel.insertResetCode(user.id, resetCode);
+
+    // Send forgot password email with reset password
+    const response = await sendForgotPasswordEmail(
+      process.env.RESEND_DEFAULT_EMAIL,
+      email,
+      resetCode
+    );
+
+    if (response && response.status === 200) {
+      return res.status(200).json({
+        message: "Reset code sent to your email. Please check your inbox.",
+      });
+    } else {
+      return res.status(500).json({ message: "Error sending reset email" });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Can't process forgot password request",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * Verifies the reset code for password reset
+ * @param {*} req The request object
+ * @param {*} res The response object
+ * @returns A success message
+ */
+export const verifyResetCode = async (req, res) => {
+  const { userID, enteredCode } = req.body;
+
+  try {
+    const resetData = await authModel.getResetCode(userID);
+
+    if (!resetData) {
+      return res.status(404).json({ message: "No reset code found" });
+    }
+
+    const { resetCode, expirationTime } = resetData;
+
+    const isValid = validateResetCode(enteredCode, resetCode, expirationTime);
+
+    if (isValid) {
+      await authModel.deleteResetCode(userID);
+      return res.status(200).json({ message: "Reset code verified" });
+    } else {
+      return res.status(400).json({ message: "Invalid or expired reset code" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Can't verify reset code", error: error.message });
+  }
+};
+
+/**
  * Logs out a user by clearing the refresh token cookie
  * @param {*} req The request object
  * @param {*} res The response object
@@ -52,10 +129,8 @@ export const loginUsingEmail = async (req, res) => {
  */
 export const logout = async (req, res) => {
   res.clearCookie("refreshToken");
-  return res
-    .status(200)
-    .json({
-      message: "Logged out successfully",
-      action: "Remove access token from client side",
-    });
+  return res.status(200).json({
+    message: "Logged out successfully",
+    action: "Remove access token from client side",
+  });
 };
