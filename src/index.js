cb1dd6e4598667be83d3f490487f4e0744c6e817
Pratim Sadhu <pratimsadhu@icloud.com>
2024-12-03 09:35:30 -0500

    Change port to 3001

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.get("/protected", authenticate, (req, res) => {
   res.json({ message: `Hello, user ${req.id}!` });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
