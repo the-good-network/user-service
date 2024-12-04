9022ea2e33e60766d6b20869ab41bafa98403c2c
Pratim Sadhu <pratimsadhu@icloud.com>
2024-12-04 13:27:31 -0500

    Moved signup and reset-password from user controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,27 +1,27 @@
 import express from "express";
-import {
-  loginUsingEmail,
-  forgotPassword,
-  verifyResetCode,
-  logout,
-  refreshTokens,
-} from "../controllers/authController.js";
+import authController from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Route to sign up a new user
+router.post("/signup", authController.signupUser);
+
 // Route to log in using email and password
-router.post("/login", loginUsingEmail);
+router.post("/login", authController.loginUsingEmail);
 
 // Route to log out and clear refresh token
-router.post("/logout", logout);
+router.post("/logout", authController.logout);
 
 // Route to initiate forgot password process by sending reset code
-router.post("/forgot-password", forgotPassword);
+router.post("/forgot-password", authController.forgotPassword);
+
+// Route to reset the user's password
+router.post("/reset-password", authController.resetPassword);
 
 // Route to verify the reset code provided by the user
-router.post("/verify-reset-code", verifyResetCode);
+router.post("/verify-reset-code", authController.verifyResetCode);
 
 // Route to refresh the access and refresh tokens
-router.post("/refresh", refreshTokens);
+router.post("/refresh", authController.refreshTokens);
 
 export default router;
