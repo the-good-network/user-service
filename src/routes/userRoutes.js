c0585de26362534c497109ea2b37078222833a12
Pratim Sadhu <pratimsadhu@icloud.com>
2024-12-04 13:14:41 -0500

    Move admin routes to user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,27 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-import { authenticate } from "../middleware/authMiddleware.js";
+import { authenticate } from "../middleware/authenticationMiddleware.js";
+import { authorize } from "../middleware/authorizationMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", userController.signupUser);
 router.post("/reset-password", userController.resetPassword);
 
-router.get("/", authenticate, userController.getOwnProfile)
+router.get("/", authenticate, userController.getOwnProfile);
+
+router.get(
+  "/users",
+  authenticate,
+  authorize(["admin"]),
+  userController.getAllUsers
+);
+
+router.get(
+  "/user/:id",
+  authenticate,
+  authorize(["admin"]),
+  userController.getUser
+);
 
 export default router;
